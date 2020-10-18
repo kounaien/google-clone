1733588d84a390e524e41bec572814b5d0bf78a9
kounaien <1127fujihiroto@gmail.com>
2020-10-19 00:33:01 +0900

    fix proplems

---
+++ memo.md
@@ -0,0 +1,5 @@
+- material ui
+- react router
+- css  
+  text-decoration: inherit;
+  min-width: 13vh;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <h1>hey clever programmer let's build the google clone</h1>
     </div>
   );
 }
