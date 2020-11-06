ce66fea02dd115c9010dddc79968f43a1f604d00
kounaien <1127fujihiroto@gmail.com>
2020-11-06 23:59:17 +0900

    jf

---
+++ src/useGoogleSearch.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import API_KEY from "./keys";
+
+const CONTEXT_KEY = "1bbebc318731edff5";
+
+function useGoogleSearch() {
+  const [data, setData] = useState(null);
+}
+
+export default useGoogleSearch;
